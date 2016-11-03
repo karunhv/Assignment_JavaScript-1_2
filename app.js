e5383f7da1625fc718d7d49fca07457fb78190fe
Karunakar HV <frontendacadglid@gmail.com>
2016-11-03 19:42:54 +0530

    JavaScript Data Types and Variables
    Create a JavaScript file app.js and include that script file into your index.html file using <script> tag. In app.js file, create an object using object literal notation that stores your name, age, dob and placeofbirth. Display the value of each property in console.

---
+++ app.js
@@ -0,0 +1,11 @@
+var PersonalDetails ={ //Object
+  name: "Karunakar",
+  age: 30,
+  dateOfBirth: "26-Sep-1986",
+  placeOfBirth: "Bangalore"
+};
+
+console.log("Name: " + PersonalDetails.name); //Printing the Object members
+console.log("Age: " + PersonalDetails.age);
+console.log("Date Of Birth: " + PersonalDetails.dateOfBirth);
+console.log("Place Of Birth: " + PersonalDetails.placeOfBirth);
